88a360ddf6a55e7de71e3e5490a3b286a7ee5a1b
Isaac Hussain <isaaclearns58@gmail.com>
2023-01-19 12:10:10 +0300

    Adds validation

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("hello world!");
+const form = document.querySelector('form');
+const btnAddNote = document.querySelector('.btn-add-note');
+
+form.addEventListener('submit', (e) => {
+    const title = document.querySelector('#title').value;
+    const note = document.querySelector('#note').value;
+    const errorElement = document.querySelector('.error-message');
+    let messages = [];
+
+
+    if (title === '' || title === null || 
+    note === '' || note === null) {
+        e.preventDefault();
+        messages.push("Please fill all fields");
+    } else {
+        
+    }
+
+    if(messages.length > 0) {
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ');
+    }
+
+    
+
+});
